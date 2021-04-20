5f80dd4ee6eb242bcd5e87410778d92caf1454e8
Felipe P Covatti <felipepcovatti@gmail.com>
2021-04-19 21:29:42 -0300

    Set global body color

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,5 +3,12 @@ import { extendTheme } from "@chakra-ui/react";
 export const theme = extendTheme({
   fonts: {
     body: 'Poppins',
+  },
+  styles: {
+    global: {
+      body: {
+        color: 'gray.700'
+      }
+    }
   }
 })
